65dd8bbc0942bc60ce484b811972467d5bfd6ac3
Shao-Ting Chiu <stchiu@email.tamu.edu>
2024-05-15 16:33:51 -0500

    wip: MMPDE 5

---
+++ code/example_2_2_2.jl
@@ -0,0 +1,10 @@
+"""
+Example 2.2.2
+"""
+
+using Pkg;
+Pkg.activate(".");
+using LinearAlgebra
+using Plots
+
+
---
+++ code/example_2_3_1.jl
@@ -0,0 +1,9 @@
+"""
+Example 2.3.1
+"""
+
+using Pkg;
+Pkg.activate(".");
+using LinearAlgebra
+using Plots
+
